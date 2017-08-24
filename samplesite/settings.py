46916ac3c931c09a1c724bb07c2f2f5448e964ed
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-24 17:22:50 -0400

    sample django size up and running with random number generator function

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.
