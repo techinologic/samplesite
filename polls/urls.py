7be3a57e0d5f5c2c71eeccc63d4a8b9525b07394
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-24 22:31:43 -0400

    added urls.py and polls.py

---
+++ polls/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+import . import views
+
+urlpatterns = [
+    url(r)
+]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("You are in the polls index")
+
 
 # Create your views here.
