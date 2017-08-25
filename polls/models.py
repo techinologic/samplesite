70efc89c600bdfbafa97c14d555e16c12174e874
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-24 22:57:50 -0400

    added stuffs in models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+from django.utils import timezone
+
+class
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You are in the polls index")
+    return HttpResponse("You are in the polls index.")
 
 
 # Create your views here.
