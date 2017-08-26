934013b37775adf31be6ae8b9cb50baae0336a4d
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-25 23:02:48 -0400

    added detail, index, and results.html

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse
 
+from .models import Question, Choice
+
+from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404
+
 def index(request):
-    return HttpResponse("You are in the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
-# Create your views here.
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question':question})
+def results(request, question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
