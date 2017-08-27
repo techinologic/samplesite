6446378fc9c967a1ee60285badca983832e337bf
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-27 12:57:37 -0400

    added detail, index, and results.html

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render
 
 from django.shortcuts import get_object_or_404
 
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
@@ -25,4 +29,19 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
