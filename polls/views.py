c1619e2f0e685bd6c18ce4e2db1de76b10a59727
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-28 19:18:29 -0400

    results.html code

--- polls/views.py
+++ polls/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseRedirect
 
 from django.urls import reverse
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
@@ -20,13 +21,15 @@ def index(request):
 
 
 def detail(request, question_id):
-
     question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {'question': question})
+
+
 def results(request, question_id):
-    response = "You are looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -42,6 +45,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
 
-        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-
-
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
--- samplesite/urls.py
+++ samplesite/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^helloworld/$', hello_world),
     url(r'^$', root_page),
     url(r'^random/(\d+)/$', random_number),
-    url(r'^polls/', include('polls.urls'))
+    url(r'^polls/', include('polls.urls')),
 
 ]
 
